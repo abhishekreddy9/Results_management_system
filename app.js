ab5b73dd750592ae38a6649bbac898f5a7e857e0
abhishekreddy9 <abhishekreddy.sunkireddy@gmail.com>
2018-06-10 18:29:36 +0530

    added menu

--- app.js
+++ app.js
@@ -13,35 +13,34 @@ var storage = multer.memoryStorage();
 var upload = multer({dest : 'uploads/'});
 var fs = require('fs');
 
- var resultSchema = new Schema({
-    hallticket:  String,
-    subject_code: String,
-    subject_name:   String,
-    internal_marks: Number,
-    external_marks: Number,
-    total_marks: Number,
-    credits: Number,
-    year: String,
+var resultSchema = new Schema({
+  hallticket:  String,
+  subject_code: String,
+  subject_name:   String,
+  internal_marks: Number,
+  external_marks: Number,
+  total_marks: Number,
+  credits: Number,
+  year: String,
 
-  });
+});
 
-var results_model = mongoose.model('results', resultSchema);
+var resultsList = new Schema({
+  listid: String,
+  year: String,
+  Regulation:String,
+  Paper:String,
+  Date:String
+});
 
-
-// results_model.insertMany( result.Sheet0 ,function(err){
-//      if (err) 
-//    console.log("err");
-//    else
-//        console.log("Saved!");
-//  });
+var results_list = mongoose.model('results_List',resultsList);
 
 app.get("/admin",function(req,res){
   res.render("admin");
-}
-);
+});
 
 app.get("/cleardb",function(req,res){
-  mongoose.connection.db.dropCollection('results', function(err, result) {
+  mongoose.connection.db.dropDatabase('data', function(err, result) {
     if(err)
       console.log(err);
     else
@@ -50,96 +49,107 @@ app.get("/cleardb",function(req,res){
 });
 
 app.post("/add",upload.array('file'),function(req,res){
-
   console.log(req.body);
   console.log(req.files);
   
+  var collName=req.body.Year+"-"+req.body.Regulation+"-"+req.body.Paper+"-"+'Results'+"-"+req.body.Date;
+  let results_model = mongoose.model(collName,resultSchema);  
+  var InsertedToList = false;
 
-  
-
-
-  req.files.forEach(function(file)
-  {
+  req.files.forEach(function(file){
     
     var result = excelToJson({
       sourceFile: file.path ,
       header:{
         // Is the number of rows that will be skipped and will not be present at our result object. Counting from top to bottom
         rows: 5 // 2, 3,les);
-    
-    },
-    columnToKey: {
-      A: 'hallticket',
-      B: 'subject_code',
-      C:'subject_name',
-      D:'internal_marks',
-      E:'external_marks',
-      F:'total_marks',
-      G:'credits',
-    }
-  });
-
-    result.Sheet0.forEach(function(years)
-         {
-           years.year=req.body.Year;
-          }
-          );
-
-     console.log(result.Sheet0);
-    results_model.insertMany( result.Sheet0 ,function(err){
-        if (err) 
-          res.send("Error");
-        else {
-          fs.unlink(file.path, function(error) {
-              if (error) {
-                console.log(error);
-              }
-              console.log('file_Deleted');
-              
-              
-              
-              
-          });
-        }
+      },
+      columnToKey: {
+        A: 'hallticket',
+        B: 'subject_code',
+        C:'subject_name',
+        D:'internal_marks',
+        E:'external_marks',
+        F:'total_marks',
+        G:'credits',
+      }
     });
-  }
-)
 
+    result.Sheet0.forEach(function(years){
+      years.year=req.body.Year;
+    });
 
-
-
-
-  
+    console.log(result.Sheet0);
+    results_model.insertMany( result.Sheet0 ,function(err){
+      if (err) 
+        res.send("Error");
+      else {
+        fs.unlink(file.path, function(error) {
+          if (error) {
+            console.log(error);
+          }
+          else {
+            console.log('file_Deleted');
+            
+            if(!InsertedToList){
+              results_list.create({
+                listid:collName,
+                year: req.body.Year,
+                Regulation:req.body.Regulation,
+                Paper:req.body.Paper,
+                date:req.body.Date
+              },function(error){
+                if (error) {
+                  console.log(error);
+                }
+                 
+              });
+              InsertedToList = true;
+            }
+            
+         //   res.render("new");
+          }
+        });
+      }
+    });
+  });  
 });
 
-app.get("/test", function(req, res){
-        //if(err){
-        //    console.log(member);
-           // console.log(err);
-       // } else {
-         res.render("test");
-        
-        
+app.get("/test/:id", function(req, res){
+  console.log(req.params.id);
+  res.render("test",{id:req.params.id});        
 });
 
 app.post("/test", function(req, res){
-  
-     results_model.find({'hallticket':req.body.hallticket_no,'year':req.body.Year},function (err, member) {
-        if(err){
+ let results_model = mongoose.model(req.body.collection_id,resultSchema);
+
+
+ results_model.find({'hallticket':req.body.hallticket_no,'year':req.body.Year},function (err, member) {
+    if(err){
       console.log(err);
-      }
-      else 
-      {
-        console.log(req.body.hallticket_no);
-        console.log(req.body.Year);
-        console.log(typeof req.body.Year);
-          res.render("test", {student: member });
+    }
+    else {
+      console.log(member);
+      console.log(req.body.hallticket_no);
+      console.log(req.body.Year);
+      console.log(typeof req.body.Year);
+      res.render("test", {student: member,id:req.body.collection_id });
           
-            }
-        });
-});    
+    }
+  });
+});
 
 
+app.get("/",function(req, res){
+  results_list.find({}, function(err, allLists){
+     if(err){
+         console.log(err);
+     } else {
+       res.render("menu",{result_data:allLists});
+       //console.log(allLists);
+     }
+  });
+});
 
 app.listen(3000, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
